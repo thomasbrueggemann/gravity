ececfdf8a04f0b67166b4598ab0fc3f36fcd43b2
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-07-24 18:47:06 +0100

    set async interrupt on pin

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use rppal::gpio::Gpio;
+use std::io::stdin;
+use rppal::gpio::{Error, Gpio, Trigger, Level};
 
-fn main() -> Result<(), rppal::gpio::Error> {
+fn main() -> Result<(), Error> {
     let gpio = Gpio::new()?;
-    let pin = gpio.get(23)?.into_input();
+    let mut input_pin = gpio.get(23)?.into_input_pullup();
 
-    println!("{}", pin.read());
+    println!("{}", input_pin.read());
+
+	input_pin.set_async_interrupt(Trigger::Both,  handle_input_change)?;
+
+    stdin().read_line(&mut String::new())?;
 
 	return Ok(());
+}
+
+fn handle_input_change(level: Level) {
+	println!("{}", level);
 }
