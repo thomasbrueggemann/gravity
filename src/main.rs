3e8df9a92d8e7b0d3e27fb8308c74e88ae168121
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-07-24 16:35:37 +0100

    Read GPIO from raspberry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use rppal::gpio::Gpio;
+
+fn main() -> Result<(), rppal::gpio::Error> {
+    let gpio = Gpio::new()?;
+    let pin = gpio.get(23)?.into_input();
+
+    println!("{}", pin.read());
+
+	return Ok(());
 }
