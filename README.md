450432f0f0d25edda5d0d0c2558ed64c797f25b5
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-07-25 14:07:44 +0100

    message pass complete serial messages for parsing

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🪐 Gravity
+
+Realtime Guitar DSP Effects with Rust
+
+## Serial communication protocol Arudino -> Raspberry
+
+| Byte | Description  |
+|------|--------------|
+| 0    | 71 (= G)     |
+| 1    | 86 (= R)     |
+| 2    | 84 (= V)     |
+| 3    | 89 (= Y)     |
+| 4    | Pot 1 Byte 1 |
+| 5    | Pot 1 Byte 2 |
+| 6    |             |
+| 7    |             |
+| 8    |             |
+| 9    |             |
+| 10   |             |
+| 11   |             |
+| 12   |             |
+| 13   |             |
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use rppal::gpio::{Error, Gpio, Level, Trigger};
 use std::time::{Duration, SystemTime};
 use std::io::{self, stdin};
 use std::thread;
+use std::sync::mpsc::{channel, Sender};
 
 fn main() -> Result<(), Error> {
     let gpio = Gpio::new()?;
@@ -9,29 +10,9 @@ fn main() -> Result<(), Error> {
 
     input_pin.set_async_interrupt(Trigger::Both, handle_input_change)?;
 
-    thread::spawn(|| {
-        let mut port = serialport::new("/dev/ttyACM0", 9_600)
-            .timeout(Duration::from_millis(100))
-            .open()
-            .expect("Failed to open port");
+	let (tx, rx) = channel::<Vec<u8>>();
 
-        let mut serial_buf: Vec<u8> = vec![0; 1000];
-        println!("Receiving data via serial port");
-
-        loop {
-            match port.read(serial_buf.as_mut_slice()) {
-                Ok(t) => {
-                    let received_bytes = serial_buf[..t].to_vec();
-
-                    let stringed_buffer = String::from_utf8(received_bytes).unwrap();
-                    println!("{}", stringed_buffer);
-                },
-                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                Err(e) => eprintln!("{:?}", e),
-            }
-        
-        }
-    });
+    thread::spawn(|| handle_serial_port(tx));
 
     stdin().read_line(&mut String::new())?;
 
@@ -44,4 +25,51 @@ fn handle_input_change(level: Level) {
         .unwrap();
 
     println!("{}: {}", epoch.as_secs(), level);
+}
+
+fn handle_serial_port(tx: Sender<Vec<u8>>) {
+	let mut port = serialport::new("/dev/ttyACM0", 9_600)
+		.timeout(Duration::from_millis(100))
+		.open()
+		.expect("Failed to open port");
+
+	let mut received_data: Vec<u8> = Vec::new();
+	let seperator: Vec<u8> = vec![71, 86, 84, 89];
+
+	println!("Receiving data via serial port");
+	let mut serial_buf: Vec<u8> = vec![0; 1000];
+
+	loop {
+		match port.read(serial_buf.as_mut_slice()) {
+			Ok(t) => {
+				received_data.extend_from_slice(&serial_buf[..t]);
+
+				let seq_start = find_subsequence(&received_data, &seperator);
+				if seq_start.is_some() {
+					let start_idx = seq_start.unwrap();
+					received_data.drain(0..start_idx);
+				}
+
+				let seq_end = find_subsequence(&received_data[seperator.len()..], &seperator);
+				if seq_end.is_some() {
+					let end_idx = seq_end.unwrap();
+
+					let seq = received_data
+						.drain(0..end_idx)
+						.as_slice()
+						.to_vec();
+					
+					tx.send(seq).unwrap();
+				}
+			},
+			Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+			Err(e) => eprintln!("{:?}", e)
+		}
+	}
+}
+
+fn find_subsequence(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    return haystack
+		.windows(needle.len())
+		.position(|window| window == needle);
 }
