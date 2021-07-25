1717144990b22c714ca7e6a83f6b7c2e2272f28b
Thomas Brüggemann <mail@thomasbrueggemann.com>
2021-07-25 10:23:23 +0100

    fix indentation

--- src/main.rs
+++ src/main.rs
@@ -9,33 +9,33 @@ fn main() -> Result<(), Error> {
 
     input_pin.set_async_interrupt(Trigger::Both, handle_input_change)?;
 
-	thread::spawn(|| {
-		let mut port = serialport::new("/dev/ttyACM0", 9_600)
-			.timeout(Duration::from_millis(100))
-			.open()
-			.expect("Failed to open port");
+    thread::spawn(|| {
+        let mut port = serialport::new("/dev/ttyACM0", 9_600)
+            .timeout(Duration::from_millis(100))
+            .open()
+            .expect("Failed to open port");
 
-		let mut serial_buf: Vec<u8> = vec![0; 1000];
-		println!("Receiving data via serial port");
+        let mut serial_buf: Vec<u8> = vec![0; 1000];
+        println!("Receiving data via serial port");
 
-		loop {
-			match port.read(serial_buf.as_mut_slice()) {
-				Ok(t) => {
-					let received_bytes = serial_buf[..t].to_vec();
+        loop {
+            match port.read(serial_buf.as_mut_slice()) {
+                Ok(t) => {
+                    let received_bytes = serial_buf[..t].to_vec();
 
-					let stringed_buffer = String::from_utf8(received_bytes).unwrap();
-					println!("{}", stringed_buffer);
-				},
-				Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-				Err(e) => eprintln!("{:?}", e),
-			}
-		
-		}
-	});
+                    let stringed_buffer = String::from_utf8(received_bytes).unwrap();
+                    println!("{}", stringed_buffer);
+                },
+                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                Err(e) => eprintln!("{:?}", e),
+            }
+        
+        }
+    });
 
-	stdin().read_line(&mut String::new())?;
+    stdin().read_line(&mut String::new())?;
 
-	return Ok(());
+    return Ok(());
 }
 
 fn handle_input_change(level: Level) {
